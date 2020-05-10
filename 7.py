18776b8f673e3a2a576de7f6fb6cabb31ef43ae3
Namemai <40319864+Namemai@users.noreply.github.com>
2020-05-10 18:12:33 +0700

    Update 7.py

--- 7.py
+++ 7.py
@@ -27,11 +27,11 @@ Bot_startTime = time.strftime("%H:%M:%S", time.localtime())
 print("""
 \033["""+str(randint(0,1))+""";"""+str(randint(31,36))+"""m[ %s Start Bot ]\033[0m    
 """%(Bot_startTime))
-cl = LINE("cws34465@bcaoo.com","mai065558mai")
+cl = LINE("06555mai@gmail.com","mai065558mai")
 cl.log("Auth Token : " + str(cl.authToken))
 channelToken = cl.getChannelResult()
 #========================Token1===================================================#
-ka = LINE("agg58835@zzrgg.com","mai065558mai")
+ka = LINE("cws34465@bcaoo.com","mai065558mai")
 ka.log("Auth Token : " + str(ka.authToken))
 channelToken = ka.getChannelResult()
 #========================Token2===================================================#
@@ -39,7 +39,7 @@ kb = LINE("kuq55542@eoopy.com","mai065558mai")
 kb.log("Auth Token : " + str(kb.authToken))
 channelToken = kb.getChannelResult()
 #========================Token3===================================================#
-kc = LINE("whm11723@eoopy.com","mai065558mai")
+kc = LINE("vmh25600@zzrgg.com","mai065558mai")
 kc.log("Auth Token : " + str(kc.authToken))
 channelToken = kc.getChannelResult()
 #========================Token4===================================================#
